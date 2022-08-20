a46bd79cee5365cc6c5cc9c33275386614981097
Oleksandra Dvorova <sashadvorovay@gmail.com>
2022-08-20 20:57:33 +0300

    New promise

--- src/index.js
+++ src/index.js
@@ -51,6 +51,7 @@ axios.get(url).then(showTemperature);
 function finding_city(city) {
   let apiUrl =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
+  a
 }
 
 //Show temperature in finding city and another info about weather
@@ -150,6 +151,7 @@ else
 {
   advice.innerHTML = "Just enjoy time"
 }
+getForecast(response.data.coord);
 }
 //For working search
 function handleSubmit(event) {
@@ -257,4 +259,47 @@ for (let y = t; y<5;y++)
       fiveth.innerHTML = weekday[d]
   }
 }
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
